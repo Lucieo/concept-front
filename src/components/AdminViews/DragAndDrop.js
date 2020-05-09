6bbccdd42e37c47420b530fecd4db5e7f567b42e
LucieO <luciecamilleory@gmail.com>
2020-05-09 15:35:05 +0200

    updated icons

--- src/components/AdminViews/DragAndDrop.js
+++ src/components/AdminViews/DragAndDrop.js
@@ -1,28 +1,11 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import conceptsList from "./concepts";
-
-/**
- * Moves an item from one list to another list.
- */
-const move = (source, destination, droppableSource, droppableDestination) => {
-  const sourceClone = Array.from(source);
-  const destClone = Array.from(destination);
-  const element = sourceClone[droppableSource.index];
-
-  destClone.splice(droppableDestination.index, 0, element);
-
-  const result = {};
-  result[droppableSource.droppableId] = sourceClone;
-  result[droppableDestination.droppableId] = destClone;
-
-  return result;
-};
-
-const grid = 8;
+import conceptsListInfo from "./concepts";
+import uuid from "react-uuid";
+import ReactTooltip from "react-tooltip";
 
 const getItemStyle = (isDragging, draggableStyle) => ({
-  // some basic styles to make the items look a bit nicer
+  ...draggableStyle,
   userSelect: "none",
   padding: 5,
   margin: 5,
@@ -30,138 +13,192 @@ const getItemStyle = (isDragging, draggableStyle) => ({
   marginLeft: 5,
   borderRadius: 5,
   boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
-  // change background colour if dragging
+  width: "100px!important",
   background: isDragging ? "lightgreen" : "white",
-  // styles we need to apply on draggables
-  ...draggableStyle,
+  width: "100px",
+  height: "100px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 });
 
 const getListStyle = (isDraggingOver) => ({
   background: isDraggingOver ? "lightblue" : "lightgrey",
-  padding: grid,
   width: "100%",
+  minHeight: 100,
+  padding: 10,
+  display: "flex",
+  flexWrap: "wrap",
 });
 
-class Test extends Component {
-  state = {
-    concepts: conceptsList,
-    concept1: [],
+const GamePanel = () => {
+  const [conceptsList, setConcepts] = useState(conceptsListInfo);
+  const [selected, setSelected] = useState([[]]);
+
+  const removeConcept = (elementIdx, lineIdx) => {
+    let newLine = [...selected[lineIdx]];
+    newLine.splice(elementIdx, 1);
+    let newSelected = [...selected];
+    newSelected[lineIdx] = newLine;
+    setSelected(newSelected);
   };
 
-  /**
-   * A semi-generic way to handle multiple lists. Matches
-   * the IDs of the droppable container to the names of the
-   * source arrays stored in the state.
-   */
+  const deleteLine = (idx) => {
+    const newlist = [...selected];
+    newlist.splice(idx, 1);
+    setSelected(newlist);
+  };
 
-  getList = (id) => this.state[id];
-
-  onDragEnd = (result) => {
+  const onDragEnd = (result) => {
     const { source, destination } = result;
-
-    // dropped outside the list
-    if (!destination) {
-      return;
+    const destinationId = destination?.droppableId;
+    if (destinationId && destinationId.indexOf("concepts") < 0) {
+      const updatedSelected = Array.from(selected[destinationId]);
+      const element = conceptsList[source.index];
+      if (updatedSelected.map((el) => el.img).indexOf(element.img) === -1) {
+        updatedSelected.push({ ...element, id: uuid() });
+      }
+      const newSelectedList = [...selected];
+      newSelectedList[destinationId] = updatedSelected;
+      setSelected(newSelectedList);
     }
-
-    if (
-      ["concept1", "concept2", "concept3"].indexOf(destination.droppableId) > -1
-    ) {
-      console.log(source.droppableId);
-      const result = move(
-        this.getList(source.droppableId),
-        this.getList(destination.droppableId),
-        source,
-        destination
-      );
-
-      this.setState({
-        concepts: result.concepts,
-        concept1: result.concept1,
-      });
-    }
+    return;
   };
 
-  // Normally you would want to split things out into separate components.
-  // But in this example everything is just done in one place for simplicity
-  render() {
-    return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <h1>SELECTED 1</h1>
-        <Droppable droppableId="concept1">
-          {(provided, snapshot) => (
-            <div
-              ref={provided.innerRef}
-              style={getListStyle(snapshot.isDraggingOver)}
-            >
-              {this.state.concept1.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided, snapshot) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      style={getItemStyle(
-                        snapshot.isDragging,
-                        provided.draggableProps.style
-                      )}
-                    >
-                      <img
-                        src={`/images/concepts/${item.img}`}
-                        style={{ width: 80, height: 80 }}
-                      />
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-        <h1>CONCEPTS</h1>
+  return (
+    <>
+      <button
+        className={`btn ${selected.length >= 4 && "disabled"}`}
+        onClick={() => selected.length < 4 && setSelected([...selected, []])}
+      >
+        Ajouter un concept
+      </button>
+      <DragDropContext onDragEnd={onDragEnd}>
+        {selected.map((el, idx) =>
+          SelectionLine(el, idx, () => deleteLine(idx), removeConcept)
+        )}
+        <h4>Les concepts</h4>
         <Droppable droppableId="concepts" isDropDisabled={true}>
-          {(provided, snapshot) => (
-            <div
-              ref={provided.innerRef}
-              style={getListStyle(snapshot.isDraggingOver)}
-            >
-              {this.state.concepts.map((item, index) => {
-                if (item.img) {
-                  return (
-                    <Draggable
-                      key={item.id}
-                      draggableId={item.id}
-                      index={index}
-                    >
-                      {(provided, snapshot) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          style={getItemStyle(
-                            snapshot.isDragging,
-                            provided.draggableProps.style
-                          )}
-                        >
-                          <img
-                            src={`/images/concepts/${item.img}`}
-                            style={{ width: 80, height: 80 }}
-                          />
-                        </div>
-                      )}
-                    </Draggable>
-                  );
-                } else {
-                  return <p>{item.text}</p>;
-                }
-              })}
-              {provided.placeholder}
-            </div>
-          )}
+          {(provided, snapshot) => {
+            console.log(provided.placeholder);
+            return (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+              >
+                {conceptsList.map((item, index) => conceptCard(item, index))}
+                {provided.placeholder}
+              </div>
+            );
+          }}
         </Droppable>
       </DragDropContext>
+    </>
+  );
+};
+
+export default GamePanel;
+
+const conceptCard = (
+  item,
+  index,
+  isLine = false,
+  removeConcept = undefined
+) => {
+  const id = item.img.split(".")[0];
+  if (item.img) {
+    return (
+      <Draggable
+        key={item.id}
+        draggableId={item.id}
+        index={index}
+        isDragDisabled={isLine}
+      >
+        {(provided, snapshot) => (
+          <>
+            <ReactTooltip
+              id={item.id}
+              place="top"
+              type="dark"
+              effect="float"
+              multiline={true}
+            />
+            <a data-for={item.id} data-tip={item.text} data-iscapture="true">
+              <div
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                style={getItemStyle(
+                  snapshot.isDragging,
+                  provided.draggableProps.style
+                )}
+              >
+                {isLine && (
+                  <span
+                    style={{
+                      cursor: "pointer",
+                      fontWeight: "bold",
+                      color: "#005236",
+                      height: "100%",
+                    }}
+                    onClick={removeConcept}
+                  >
+                    X
+                  </span>
+                )}
+                <img
+                  src={`/images/concepts/${item.img}`}
+                  style={{ width: 80, height: 80 }}
+                />
+              </div>
+            </a>
+            {snapshot.isDragging && (
+              <div style={getItemStyle(snapshot.isDragging)}>
+                <img
+                  src={`/images/concepts/${item.img}`}
+                  style={{ width: 80, height: 80 }}
+                />
+              </div>
+            )}
+          </>
+        )}
+      </Draggable>
+    );
+  } else {
+    return (
+      <p style={{ width: "100%" }} key={index}>
+        {item.text}
+      </p>
     );
   }
-}
+};
 
-export default Test;
+const SelectionLine = (lineInfo, idx, deleteAction, removeConcept) => {
+  return (
+    <div key={idx}>
+      <h4>
+        <button
+          style={{ padding: "0 10px", borderRadius: "50%" }}
+          className="btn outlined"
+          onClick={deleteAction}
+        >
+          <i className="material-icons">delete</i>
+        </button>
+        {idx === 0 ? " Concept Principal" : ` Concept ${idx + 1}`}
+      </h4>
+      <Droppable droppableId={`${idx}`}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            style={getListStyle(snapshot.isDraggingOver)}
+          >
+            {lineInfo.map((item, index) =>
+              conceptCard(item, index, true, () => removeConcept(index, idx))
+            )}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </div>
+  );
+};
